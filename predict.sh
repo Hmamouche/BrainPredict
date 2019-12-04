2fe84dc1a9adb31432ae8cca80982752420825f7
youssef <hmamoucheyussef@gmail.com>
2019-12-04 15:23:41 +0100

    update src

--- predict.sh
+++
@@ -1,8 +0,0 @@
-# Generate time series
-python src/predict.py -rg 1 2 3 4 5 6 -t r  -ofp ../../OpenFace -pmp PredictionModule -in Demo -g
-# Make predictions
-python src/predict.py -rg 1 2 3 4 5 6 -t r  -ofp ../../OpenFace -pmp PredictionModule -in Demo -p
-# Generate time series video from the obtained predictions
-python src/animation.py -in Demo
-# using visbrain to visulize the prediction in the brain
-python src/visualization.py -in Demo
