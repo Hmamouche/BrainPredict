0c6fcf24b4cde41621ce42bcbcf7ba70fd95c1df
youssef <hmamoucheyussef@gmail.com>
2020-04-17 16:29:53 +0200

    update models

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 # Generate time series
-python src/generate_time_series.py -rg 1 2 3 4 5 6 -pmp PredictionModule -in Demo -ofp ../../OpenFace
+#python src/generate_time_series.py -rg 1 2 3 4 5 6 -pmp PredictionModule -in Demo -ofp ../../OpenFace
 
 
 # Make predictions
-python src/predict.py -rg 1 2 3 4 5 6 -pmp PredictionModule -in Demo -t r
+python src/predict.py -rg 1 2 3 4 5 6 -pmp PredictionModule -in Demo -t h
 
 
 # Generate time series video from the obtained predictions
